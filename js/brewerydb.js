24af8156e33ad5891e39fd0ee4ae6947a4ac686a
Mark-A-A <markawai01@gmail.com>
2015-12-17 20:14:50 -0500

    fixed identation, added js links, added brewery API AJAX call

--- js/brewerydb.js
+++ js/brewerydb.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+  $.ajax({
+    type: "Get",
+    url: "http://api.brewerydb.com/v2/?key=4d31671ed97df8dccb12177e18f94199",
+    success: function(drinks){
+      console.log(drinks);
+    },
+    //error: function (jqXHR, textStatus, errorThrown) {
+      console.log("something went wrong");
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    },
+  })
+});
+
 API Key:  4d31671ed97df8dccb12177e18f94199
