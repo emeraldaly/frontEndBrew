72901906f7dfb80d5810109dacf345e89736805c
Mark-A-A <markawai01@gmail.com>
2015-12-17 21:29:34 -0500

    breweryAPI endpoint

--- js/brewerydb.js
+++ js/brewerydb.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
   $.ajax({
     type: "Get",
-    url: "http://api.brewerydb.com/v2/?key=4d31671ed97df8dccb12177e18f94199",
+    //url: "http://api.brewerydb.com/v2/?key=4d31671ed97df8dccb12177e18f94199",
+    url: "https:crossorigin.me/http://api.brewerydb.com/v2/?key=4d31671ed97df8dccb12177e18f94199",
     success: function(drinks){
-      console.log(drinks);
+      
+      for(var i =0; i< drinks.length; i++){
+        debugger
+        console.log(drinks[i]);
+      }
     },
-    //error: function (jqXHR, textStatus, errorThrown) {
+    error: function (jqXHR, textStatus, errorThrown) {
       console.log("something went wrong");
       console.log(jqXHR);
       console.log(textStatus);
-      console.log(errorThrown);
+      console.log(errorThrown); 
     },
-  })
+  });
 });
 
-API Key:  4d31671ed97df8dccb12177e18f94199
+//API Key:  4d31671ed97df8dccb12177e18f94199
