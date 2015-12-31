2d889fe1761a9e8316372df24420bd83d1a72a94
Mark-A-A <markawai01@gmail.com>
2015-12-30 22:17:35 -0500

    started psuedo-coding out the search URLs since AJAX calls don't work with BreweryDB for security reasons. Will do searches for location State and Zipcode. Also started function to append returned API object data

--- js/brewerydb.js
+++ js/brewerydb.js
@@ -1,11 +1,49 @@
 $(document).ready(function(){
 
-  var brewDBAPI = "https:crossorigin.me/http://api.brewerydb.com/v2/";
-  brewDBAPI += "?key=4d31671ed97df8dccb12177e18f94199";
-  //brewDBAPI += "/beer/random";
-  //http://api.brewerydb.com/v2//search?q=%2218%22+Imperial+IPA&key=4d31671ed97df8dccb12177e18f94199
+//var userBeerNameSearch = $()
 
 
+//Will use specific API endpoints to run searches
+
+//General Search URL:
+//http://api.brewerydb.com/v2/search?q=60%2Bminute&type=beer&key=4d31671ed97df8dccb12177e18f94199
+
+
+//Search Beer By Name:
+//https://api.brewerydb.com/v2/beers?name=60%20minute%20IPA&key=4d31671ed97df8dccb12177e18f94199
+// %20 = space
+
+
+//Locations
+//By State
+//https://api.brewerydb.com/v2/locations?state=new%20york%20&key=4d31671ed97df8dccb12177e18f94199
+
+//By Zip Code
+//https://api.brewerydb.com/v2/locations?postalCode=10469&key=4d31671ed97df8dccb12177e18f94199
+
+
+//*Not working* = URL for Location Search: api.brewerydb.com/v2/breweries?key=XXXXXX&withLocations=Y
+
+
+
+//Breweries by Beer Search Example: Coors Light
+ //http://api.brewerydb.com/v2/beers?name=coors%20light&withBreweries=Y&key=4d31671ed97df8dccb12177e18f94199
+
+
+//A Function To Run the Search based on the URL we specify
+  function breweryDBurlAPI () {
+
+    var brewDBAPI = "https:crossorigin.me/http://api.brewerydb.com/v2/";
+    //brewDBAPI += what user sets to search by
+    //brewDBAPI += what user sets to search by
+    brewDBAPI += "?key=4d31671ed97df8dccb12177e18f94199";
+    //brewDBAPI += "/beer/random";
+    //http://api.brewerydb.com/v2/search?q=%2218%22+Imperial+IPA&key=4d31671ed97df8dccb12177e18f94199
+
+
+  };
+
+//AJAX Call to access the endpoint
   $.ajax({
     type: "Get",
     dataType: 'json',
@@ -32,7 +70,28 @@ $(document).ready(function(){
       console.log(textStatus);
       console.log(errorThrown); 
     }
-  });
-});
+  }); //End of AJAX call
+
+//Function to Append the results from the API 
+  for (var i = 0; i < window.brews.length; i++) {
+    var brewRow = brewBuild(window.brews[i]);
+    console.log(brewCol);
+    $(".search panel").append(tweetCol)
+  };
+
+  function brewBuild(brewResults) {
+    var brewColDiv = $("<div>").addClass("col-md-4");
+    var brewPictureDiv = $("<div>").addClass("picture");
+    var brewPic = $("<img>").attr("src",  brewResults.data.images.medium)
+    var brewName;
+    var brewLocation;
+    //var  blah blah;
+
+    console.log(brewPic);
+    console.log(brewColDiv);
+    colDiv.append(brewPic);
+
+    return colDiv;
+  }; //end of function
 
 //API Key:  4d31671ed97df8dccb12177e18f94199
