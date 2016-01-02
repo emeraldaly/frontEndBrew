761b057d457487e12aa41ee617e69e0f1cf92b91
Mark-A-A <markawai01@gmail.com>
2016-01-02 15:56:45 -0500

    Search Buttons added with Event Listeners. Need to save user input as variables for search

--- js/searches.js
+++ js/searches.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   //Testing Search Now Button 
-  $(".search").on("click", function(e){
-    //e.preventDefault();
-    alert("search button works");
+  $(".search1").on("click", function(e){
+    e.preventDefault();
+    alert("search button 1 works");
+    var beerName = $("#beerNameSearch1").val();
+    console.log(beerName);
+    alert(beerName);
   });
 
 //var userBeerNameSearch = $()
