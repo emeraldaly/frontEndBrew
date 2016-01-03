2be1cb795870b77172b88e9ed822bbce51734069
ctevnan <carolyntevnan@yahoo.com>
2016-01-02 21:31:34 -0500

    accessing uber api is going slowly. added map on swoop.html. trying to add uber button beneath map but some small error is fighting me

--- js/productsearch.js
+++ js/productsearch.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-   alert("hello world");
+   $(".btn-default").on("click", function(e){
+    e.preventDefault();
+    var userAddress = $("#userAddress").val();
+
+    alert(userAddress);
+   })
 });
