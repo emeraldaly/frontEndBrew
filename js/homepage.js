2b8c2c4dab0dae0f167aefd8b6085d700f148927
Mark-A-A <markawai01@gmail.com>
2016-01-02 19:56:10 -0500

    cleaned up homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  $(document).ready(function (){
 
   $('.carousel').carousel({
@@ -6,10 +6,3 @@
   });     //End of Carousel JS
 
  });      //End of Document CallBack
-=======
- $(document).ready(function(){
-  $('.carousel').carousel({
-          interval: 2000
-      });
-});
->>>>>>> c2fa0623bc53c639b24d7b695a1a8d6f4ca7c129
