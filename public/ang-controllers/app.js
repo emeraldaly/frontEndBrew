ce98bd37cf69d021430c4a1af59984d7091c605b
Mark-A-A <markawai01@gmail.com>
2016-04-25 05:02:49 -0400

    templating errors. working on rednering home partial and login partial

--- public/ang-controllers/app.js
+++ public/ang-controllers/app.js
@@ -1,4 +1,4 @@
-var brewineApp = angular.module("brewineApp", []);
+var brewineApp = angular.module("brewineapp", ['ui.router']);
 
 brewineApp.service('sharedProperties', function() {
   var user = 'test string value';
@@ -14,11 +14,44 @@ brewineApp.service('sharedProperties', function() {
 });
 
 
-brewineApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+brewineApp.config( ['$stateProvider', '$urlRouterProvider', '$locationProvider'], function ($stateProvider, $urlRouterProvider, $locationProvider) {
    
    $urlRouterProvider.otherwise("/"); //provide for routes that don't exist
 
    //use stateProvider to template our routes
+
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/home.html'
+    });
+
+    // .state('registration', {
+    //   url: '/register',
+    //   templateUrl: '/views/register.html'
+    // })
+
+    // .state('login', {
+    //   url: '/login',
+    //   templateUrl: '/views/login.html'
+    // })
+
+    // .state('uberRide', {
+    //   url: '/uberRide',
+    //   templateUrl: '/views/uberRide.html'
+    // })
+
+    // .state('contact', {
+    //   url: '/contact',
+    //   templateUrl: '/views/contact.html'
+    // })
+
+    // .state('about', {
+    //   url: '/about',
+    //   templateUrl: '/views/about.html'
+    // });
+
 });
   
 
